46b79b6ffeccdcedad240a45640e8038a56eaf98
robertovargash <robertovargashdez@gmail.com>
2022-10-28 09:03:36 -0400

    Add project files.

---
+++ Database2/dbo/Tables/Gateways.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Gateways] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [SerialNumber] NVARCHAR (MAX) NOT NULL,
+    [Name]         NVARCHAR (MAX) NOT NULL,
+    [IPv4Address]  NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Gateways] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Database2/dbo/Tables/Peripherals.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Peripherals] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Vendor]      NVARCHAR (MAX) NOT NULL,
+    [DateCreated] DATETIME2 (7)  NOT NULL,
+    [Status]      INT            NOT NULL,
+    [GatewayId]   INT            NOT NULL,
+    CONSTRAINT [PK_Peripherals] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Peripherals_Gateways_GatewayId] FOREIGN KEY ([GatewayId]) REFERENCES [dbo].[Gateways] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Peripherals_GatewayId]
+    ON [dbo].[Peripherals]([GatewayId] ASC);
+
